a008374538b0ecb17cdb2163a74b361a5b7d0d90
adahung <adaihung@yahoo.com.tw>
2014-04-13 01:09:18 +0800

    add test html, snapshot button and overlay

---
+++ js/snapshot.js
@@ -0,0 +1,27 @@
+YUI().use('node', 'json-stringify', function(Y) {
+	Y.one('#snapshot').on('click', function(e) {
+		snapshot();
+	});
+
+	function snapshot() {
+		html2canvas(document.body, {
+			onrendered: function(canvas) {
+		    	afterSnapshot(canvas);
+			}
+		});
+	}
+
+	function afterSnapshot(canvas) {
+		var canvasObj = {},
+			overlay;
+		canvasObj.dataUri = canvas.toDataURL();
+		canvasObj.height = canvas.getAttribute('height');
+		canvasObj.width = canvas.getAttribute('width');
+
+		// show overlay with the snapshot
+		overlay = Y.one('#snapshot-overlay');
+		overlay.one('img').setAttribute('src', canvasObj.dataUri);
+		overlay.setStyle('display', 'block');
+	}
+
+});
